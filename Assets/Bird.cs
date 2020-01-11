48907bd1f192e29529f6b442623a270577ead2a2
DAVICORREIA21 <davicorreia21@gmail.com>
2020-01-11 18:08:12 -0300

    Scripts Bird e Pipes add

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float Speed = 2f;
+    private Rigidbody2D rig;
+
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0)){
+            rig.velocity = Vector2.up * Speed;
+        }
     }
 }
---
+++ Assets/Pipes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+    public float Speed = 2f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * Speed * Time.deltaTime;
+    }
+}
